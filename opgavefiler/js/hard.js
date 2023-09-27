29abd5aebcea6f0a897d3a77bd0f08a18cebc649
sachariase99 <126781566+sachariase99@users.noreply.github.com>
2023-09-27 13:26:43 +0200

    Update hard.js

--- opgavefiler/js/hard.js
+++ opgavefiler/js/hard.js
@@ -1,52 +1,74 @@
+let userData; // Global variabel til at gemme brugerdata
+
 fetch('https://dummyjson.com/users')
-            .then(response => response.json())
-            .then(data => {
-                // Initialiser opgave 1
-                displayUsers(data.users, 'opgave1');
-            })
-            .catch(error => {
-                console.error('Fejl ved hentning af data:', error);
-            });
+    .then(response => response.json())
+    .then(data => {
+        // Gem brugerdata i den globale variabel
+        userData = data;
 
-        // Funktion til at vise brugerdata
-        function displayUsers(users, opgaveOne) {
-            const opgaveOneElement = document.getElementById(opgaveOne);
+        // Initialiser opgave 1
+        displayUsers(userData.users, 'opgave1');
+    })
+    .catch(error => {
+        console.error('Fejl ved hentning af data:', error);
+    });
 
-            // Fjern eksisterende indhold, hvis der er noget
-            opgaveOneElement.innerHTML = '';
+// Funktion til at vise brugerdata
+function displayUsers(users, opgaveOne) {
+    const opgaveOneElement = document.getElementById(opgaveOne);
 
-            // Opret en ny ul (unordered list)
-            const userList = document.createElement('ul');
+    // Fjern eksisterende indhold, hvis der er noget
+    opgaveOneElement.innerHTML = '';
 
-            // Gennemgå hver bruger og tilføj et li (list item) element til ul
-            users.slice(0, 30).forEach(user => {
-                const listItem = document.createElement('li');
-                listItem.innerHTML = `
-                    Fornavn: ${user.firstName}, Efternavn: ${user.lastName}
-                    <button onclick="displayUserDetails(${JSON.stringify(user)}, '${opgaveOne}')">Vis</button>
-                `;
-                userList.appendChild(listItem);
-            });
+    // Opret en ny ul (unordered list)
+    const userList = document.createElement('ul');
 
-            // Tilføj ul til opgaveOneElement
-            opgaveOneElement.appendChild(userList);
+    // Gennemgå hver bruger og tilføj et li (list item) element til ul
+    users.slice(0, 30).forEach((user, index) => {
+        const listItem = document.createElement('li');
+        
+        // Opret en knap til at vise detaljer om brugeren
+        const button = document.createElement('button');
+        button.textContent = 'Vis';
+        button.addEventListener('click', () => displayUserDetails(user, opgaveOne));
+        
+        // Tilføj knappen til listeelementet
+        listItem.innerHTML = `Fornavn: ${user.firstName}, Efternavn: ${user.lastName}`;
+        listItem.appendChild(button);
+        
+        userList.appendChild(listItem);
+    });
+
+    // Tilføj ul til opgaveOneElement
+    opgaveOneElement.appendChild(userList);
+}
+
+// Funktion til at vise detaljer om en bruger
+function displayUserDetails(user, opgaveOne) {
+    const opgaveOneElement = document.getElementById(opgaveOne);
+
+    // Fjern eksisterende indhold, hvis der er noget
+    opgaveOneElement.innerHTML = '';
+
+    // Opret en div til at vise detaljer om brugeren
+    const userDetails = document.createElement('div');
+
+    // Gennemgå brugerens egenskaber og opret et HTML-element for hver
+    for (const key in user) {
+        if (user.hasOwnProperty(key)) {
+            const value = user[key];
+            const propertyElement = document.createElement('p');
+            propertyElement.innerHTML = `<strong>${key}:</strong> ${value}`;
+            userDetails.appendChild(propertyElement);
         }
+    }
 
-        // Funktion til at vise detaljer om en bruger
-        function displayUserDetails(user, opgaveOne) {
-            const opgaveOneElement = document.getElementById(opgaveOne);
-
-            // Fjern eksisterende indhold, hvis der er noget
-            opgaveOneElement.innerHTML = '';
-
-            // Opret en div til at vise detaljer om brugeren
-            const userDetails = document.createElement('div');
-            userDetails.innerHTML = `
-                Fornavn: ${user.firstName}<br>
-                Efternavn: ${user.lastName}<br>
-                <button onclick="displayUsers(data.users, '${opgaveOne}')">Tilbage</button>
-            `;
-
-            // Tilføj detaljerne til opgaveOneElement
-            opgaveOneElement.appendChild(userDetails);
-        }
+    // Tilføj tilbageknappen
+    const backButton = document.createElement('button');
+    backButton.textContent = 'Tilbage';
+    backButton.addEventListener('click', () => displayUsers(userData.users, opgaveOne));
+    userDetails.appendChild(backButton);
+ 
+    // Tilføj detaljerne til opgaveOneElement
+    opgaveOneElement.appendChild(userDetails);
+}
